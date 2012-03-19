88bcf9bed7437c7a26ea82fdf404f0b102e6bc2b
Rainfur <mudmander@gmail.com>
2012-03-19 16:59:57 -0700

    Update src/net/milkycraft/PluginWrapper.java

--- src/net/milkycraft/PluginWrapper.java
+++ src/net/milkycraft/PluginWrapper.java
@@ -5,21 +5,26 @@ import net.milkycraft.Configuration.GoldConfiguration;
  * This class wraps JavaPlugin
  */
 public class PluginWrapper extends org.bukkit.plugin.java.JavaPlugin {
+    
     private GoldConfiguration config;
 
     public GoldConfiguration getConfig() {
         if(config == null) {
+            
             config = new GoldConfiguration(this);
         }
         return config;
     }
     public void reloadConfig() {
+        
         getConfig().load();
     }
     public void saveConfig() {
+        
         getConfig().save();
     }
     public void saveDefaultConfig() {
+        
         getConfig().saveDefaults();
     }
 }
