b24ccfab1d5ed9e901e381d7d41370d51d35b16d
Nick Porillo <milkywayz@mail.com>
2012-03-19 19:54:01 -0400

    Comment and removed wrongly placed error message

--- src/net/milkycraft/PluginWrapper.java
+++ src/net/milkycraft/PluginWrapper.java
@@ -1,13 +1,14 @@
 package net.milkycraft;
 
 import net.milkycraft.Configuration.GoldConfiguration;
-
+/*
+ * This class wraps JavaPlugin
+ */
 public class PluginWrapper extends org.bukkit.plugin.java.JavaPlugin {
     private GoldConfiguration config;
 
     public GoldConfiguration getConfig() {
         if(config == null) {
-        	System.err.println("[GoldTools] Config not found, generating a new one");
             config = new GoldConfiguration(this);
         }
         return config;
