5fd2cbe01f57e2987910ad1e21d1cf1fa0a14e0c
Nick Porillo <milkywayz@mail.com>
2012-03-19 22:34:29 -0400

    Small change

--- src/net/milkycraft/Configuration/Configuration.java
+++ src/net/milkycraft/Configuration/Configuration.java
@@ -4,11 +4,9 @@ import net.milkycraft.Goldtools;
 
 public class Configuration {
 	private Goldtools plugin;
-
 	public Configuration(Goldtools plugin){
 		this.plugin = plugin;
 	}
-
 	public void create(){
 		plugin.getConfig().loadDefaults(plugin.getResource("config.yml"));
 		if(!plugin.getConfig().fileExists() || !plugin.getConfig().checkDefaults()){
